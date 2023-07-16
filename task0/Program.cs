b5cc22ca05171b4046e862dfb4912a618a90a83c
Nikita <Nikita.kuzin.88@mail.ru>
2023-07-16 15:37:52 +0500

    Написали программу которая на вход принимает два числа и выдает какое больше

---
+++ task0/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.WriteLine("Это программа, которая на вход принимает два числа и выдаёт, какое число большее из двух.");
+Console.Write("введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 < number2){
+    Console.WriteLine($"Максимальное число из двух {number2}");
+}
+if (number1 > number2)
+{
+    Console.WriteLine($"Максимальное число из двух {number1} ");
+} 
+else {
+    Console.WriteLine("Вы ввели неверные данные");
+}
